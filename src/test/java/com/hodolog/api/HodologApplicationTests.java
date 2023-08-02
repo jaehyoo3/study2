10b761dcf8ca45b8d84261d3979fe8e836b07eff
usdev <usdev@naver.com>
2023-08-02 18:35:37 +0900

    test1

--- src/test/java/com/hodolog/api/HodologApplicationTests.java
+++ src/test/java/com/hodolog/api/HodologApplicationTests.java
@@ -1,13 +1,13 @@
-package com.hodolog.api;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class HodologApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.hodolog.api;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class HodologApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
