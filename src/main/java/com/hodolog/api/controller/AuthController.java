4413877974085369eb8365a33f8ce16e74fd5751
usdev <usdev@naver.com>
2023-08-03 17:30:01 +0900

    스프링 시큐리티

--- src/main/java/com/hodolog/api/controller/AuthController.java
+++ src/main/java/com/hodolog/api/controller/AuthController.java
@@ -4,10 +4,7 @@ import com.hodolog.api.request.Signup;
 import com.hodolog.api.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -15,9 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
     private final AuthService authService;
-
     @PostMapping("/signup")
     public void signup(@RequestBody Signup signup) {
         authService.signup(signup);
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "로그인 페이지입니다.";
     }
 }
---
+++ src/main/java/com/hodolog/api/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.hodolog.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping("/")
+    public String main() {
+        return "메인입니다.";
+    }
+}
